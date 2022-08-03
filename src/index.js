b61036bc4b0478238b5047e6cbb24b72419fd8fc
goteusz-maszyk <mateusz.goszyk@gmail.com>
2022-08-03 12:13:24 +0200

    some improvements to http server

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
-const { Client, ApplicationCommandType, ActivityType } = require('discord.js');
+const { Client, ApplicationCommandType, ActivityType, OAuth2Scopes } = require('discord.js');
 const config = require('./config');
 const fs = require('fs')
+const https = require('https');
 require('dotenv').config()
 
 const { intents, token } = config;
@@ -56,9 +57,21 @@ process.on('exit', () => {
 
 const server = require('http').createServer().listen(8080);
 server.on('request', (req, res) => {
+  console.log("Running " + req.method + " \"" + req.url + "\" for " + req.socket.remoteAddress + " at " + new Date())
+  if(req.url == "/invite") {
+    res.writeHead(302, {
+      'Location': client.generateInvite({scopes: [OAuth2Scopes.Bot, OAuth2Scopes.ApplicationsCommands], permissions: ['Administrator']})
+    });
+    res.end();
+    return
+  }
   res.write("I'm alive\nPath: ");
   res.write(req.url)
   res.end();
 })
+
+setInterval(() => {
+  https.request("https://Discord-AutoRoles.goteuszmaszyk.repl.co")
+}, 60 * 1000)
 
 client.login(token);
